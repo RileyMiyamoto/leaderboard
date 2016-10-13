710c52362c758c9fb44d26007d0d60675abba400
RileyMiyamoto <rkam9@hawaii.edu>
2016-10-13 07:33:11 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,4 +1,32 @@
 /**
  * Created by Akira on 10/11/2016.
  */
-console.log("Hello world");
+PlayerList = new Mongo.Collection('plauers');
+
+if(Meteor.isServer){
+
+}
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayerList.find();
+    },
+    'selectedClass': function() {
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer) {
+        return "selected"
+      }
+    }
+
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+
+
